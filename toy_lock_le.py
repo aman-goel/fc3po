4d7ebfba055ed5e79179adff872af1c2d5432891
Aman Goel <amangoel@umich.edu>
2020-03-04 11:34:55 -0500

    Adds toy lock with le

--- toy_lock_le.py
+++ toy_lock_le.py
@@ -1,11 +1,10 @@
 
 from __future__ import print_function
 import copy
+import operator
 
 numN = 2
-numE = 5
-
-ZERO = 0
+numE = 3
 
 outFile = "out"
 outF = None
@@ -13,6 +12,36 @@ def fprint(s):
     global outF
     outF.write(s + "\n")
 
+# Python function to print permutations of a given list 
+def permutation(lst): 
+  
+    # If lst is empty then there are no permutations 
+    if len(lst) == 0: 
+        return [] 
+  
+    # If there is only one element in lst then, only 
+    # one permuatation is possible 
+    if len(lst) == 1: 
+        return [lst] 
+  
+    # Find the permutations for lst if there are 
+    # more than 1 characters 
+  
+    l = [] # empty list that will store current permutation 
+  
+    # Iterate the input(lst) and calculate the permutation 
+    for i in range(len(lst)): 
+       m = lst[i] 
+  
+       # Extract lst[i] or m from the list.  remLst is 
+       # remaining list 
+       remLst = lst[:i] + lst[i+1:] 
+  
+       # Generating all permutations where m is first 
+       # element 
+       for p in permutation(remLst): 
+           l.append([m] + p) 
+    return l 
 
 class State():
     def __init__(self, state=None):
@@ -26,9 +55,10 @@ class State():
         self.held = copy.deepcopy(state.held)
         self.transfer = copy.deepcopy(state.transfer)
         self.locked = copy.deepcopy(state.locked)
+        self.le = copy.deepcopy(state.le)
 
     def __key(self):
-        return (str(self.ep), str(self.held), str(self.transfer), str(self.locked))
+        return (str(self.ep), str(self.held), str(self.transfer), str(self.locked), str(self.le))
 
     def __hash__(self):
         return hash(self.__key())
@@ -38,19 +68,26 @@ class State():
                 self.ep == other.ep and
                 self.held == other.held and
                 self.transfer == other.transfer and
-                self.locked == other.locked)
+                self.locked == other.locked and
+                self.le == other.le)
 
     def reset(self):
-        self.ep = [ZERO for i in range(numN)]
+        self.ep = [0 for i in range(numN)]
         self.held = [False for i in range(numN)]
         self.transfer = [[False for j in range(numN)] for i in range(numE)]
         self.locked = [[False for j in range(numN)] for i in range(numE)]
+        self.le = [[False for j in range(numE)] for i in range(numE)]
     
-    def update_init(self, first, e):
-        assert(e != ZERO)
+    def update_init(self, first, e, order):
         self.reset()
+        self.ep = [order[0] for i in range(numN)]
         self.ep[first] = e
         self.held[first] = True
+        for i in range(len(order)):
+            ei = order[i]
+            for j in range(i, len(order)):
+                ej = order[j]
+                self.le[ei][ej] = True
     
     def str(self, prefix="\t"):
         res = ""
@@ -61,19 +98,30 @@ class State():
         res += "\n"
         res += prefix + "ep: "
         for n in range(numN):
-            res += "N" + str(n) + ":" + str(self.ep[n]) + "  "
+            res += "N" + str(n) + ":E" + str(self.ep[n]) + "  "
         res += "\n"
         res += prefix + "transfer: "
         for e in range(numE):
             for n in range(numN):
                 if self.transfer[e][n]:
-                    res += "<" + str(e) + ",N" + str(n) + ">  "
+                    res += "<E" + str(e) + ",N" + str(n) + ">  "
         res += "\n"
         res += prefix + "locked: "
         for e in range(numE):
             for n in range(numN):
                 if self.locked[e][n]:
-                    res += "<" + str(e) + ",N" + str(n) + ">  "
+                    res += "<E" + str(e) + ",N" + str(n) + ">  "
+        res += "\n"
+        res += prefix + "le: "
+        ordering = {}
+        for e in range(numE):
+            ordering[e] = numE
+        for e1 in range(numE):
+            for e2 in range(numE):
+                if self.le[e1][e2]:
+                    ordering[e1] -= 1
+        for e, v in sorted(ordering.items(), key=operator.itemgetter(1)):
+            res += "E%d < " % e
         res += "\n"
         return res
     
@@ -83,13 +131,16 @@ class State():
             res += "held(N%d) " % n
         for n in range(numN):
             for e in range(numE):
-                res += "ep(N%d)=%d " % (n, e)
+                res += "ep(N%d)=E%d " % (n, e)
         for e in range(numE):
             for n in range(numN):
-                res += "transfer(%d,N%d) " % (e, n)
+                res += "transfer(E%d,N%d) " % (e, n)
         for e in range(numE):
             for n in range(numN):
-                res += "locked(%d,N%d) " % (e, n)
+                res += "locked(E%d,N%d) " % (e, n)
+        for e1 in range(numE):
+            for e2 in range(numE):
+                res += "le(E%d,E%d) " % (e1, e2)
         return res
         
     def str_espresso(self):
@@ -105,6 +156,9 @@ class State():
         for e in range(numE):
             for n in range(numN):
                 res += "1 " if self.locked[e][n] else "0 "
+        for e1 in range(numE):
+            for e2 in range(numE):
+                res += "1 " if self.le[e1][e2] else "0 "
         return res
         
 
@@ -116,7 +170,7 @@ class System():
     def grant(self, state, n1, n2, e):
         if not state.held[n1]:
             return False, state
-        if e <= state.ep[n1]:
+        if state.le[e][state.ep[n1]]:
             return False, state
         
         dest = State(state)
@@ -130,7 +184,7 @@ class System():
         
         dest = State(state)
         dest.transfer[e][n] = nondet
-        if e > state.ep[n]:
+        if not state.le[e][state.ep[n]]:
             dest.held[n] = True
             dest.ep[n] = e
             dest.locked[e][n] = True
@@ -141,14 +195,14 @@ class System():
 
         print("adding init")
         for first in range(numN):
-            e = 1
-#             for e in range(numE):
-#                 if e != ZERO:
-            init_state = State()
-            init_state.update_init(first, e)
-            q.append(init_state)
-            print("init: \n%s" % init_state.str())
-        
+            epochs = [i for i in range(numE)]
+            for order in permutation(epochs):
+                for nz in range(1, len(order)):
+                    init_state = State()
+                    init_state.update_init(first, order[nz], order)
+                    q.append(init_state)
+                    print("init: \n%s" % init_state.str())
+#         assert(0)
         count = 0
         while len(q) != 0:
             count += 1
@@ -177,9 +231,9 @@ class System():
         
     def print_R_espresso(self):
         global outF, outFile
-        outFile = "toy_lock_%dN_%dE" % (numN, numE)
+        outFile = "toy_lock_le_%dN_%dE" % (numN, numE)
         
-        numCol = numN + numN*numE*3
+        numCol = numN + numN*numE*3 + numE*numE
 
         outF = open("pla/"+outFile+".pla", 'w')
         fprint("# toy lock: %d nodes, %d epochs" % (numN, numE))
@@ -193,7 +247,7 @@ class System():
         fprint(".e")
         fprint("")
         print("Now run espresso with the following command:")
-        print("./espresso/espresso.linux -o eqntott pla/%s.pla > txt/%s.txt" % (outFile, outFile))
+        print("./espresso/espresso.linux -D exact -o eqntott pla/%s.pla > txt/%s.txt" % (outFile, outFile))
             
 
 def main():
