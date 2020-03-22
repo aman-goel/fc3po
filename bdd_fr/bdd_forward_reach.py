a8efaf715e1497362b508fa86dd2b74f0610509f
zzephaniahh <zeph@umich.edu>
2020-03-22 17:41:51 -0400

    Working bbd_fr

--- bdd_fr/bdd_forward_reach.py
+++ bdd_fr/bdd_forward_reach.py
@@ -4,18 +4,11 @@
 ## python.
 
 ## Import the repycudd module
-import repycudd
-import numpy as np
-
-import re
 
 class parser():
     def __init__(self):
         self.nothing = []
-
-
-    # def __init__(self):
-    # return
+        self.data_location = "data/client_server/"
 
     def read_pla(self, file_id):
         # this function extracts the variables and cubes from a .pla file
@@ -77,16 +70,13 @@ class parser():
                 continue
             if str(var) not in unique_file_names_set:
                 unique_file_names_set.add(str(var))
-        #print(unique_file_names_set)
 
         unique_file_names_list = list(unique_file_names_set) # Change the type so we can iterate
         for file_name in unique_file_names_list:
-            file_id = open("data/"+file_name+".pla","r")
+            file_id = open(self.data_location+file_name+".pla","r")
             [cubes_new, variable_name_list] = self.read_pla(file_id) # grab the names from the init file
             cubes.append(cubes_new)
             variable_names.append(variable_name_list)
-        #print(variable_names)
-
         return cubes, variable_names
 
 
@@ -97,6 +87,9 @@ class forward_reachability():
     def __init__(self):
         self.mgr = repycudd.DdManager()
         self.name2bdd = {}
+        # Initialize the present and next state dd arrays
+        self.present_state_DDArray = repycudd.DdArray(self.mgr, 2)
+        self.next_state_DDArray = repycudd.DdArray(self.mgr, 2)
 
     def print_dd (self, dd, n, pr ):
         #self.print_dd(source,2,2)
@@ -104,7 +97,7 @@ class forward_reachability():
         print("DdManager vars: ", self.mgr.ReadSize() );                  # /*Returns the number of BDD variables in existence*/
         print("DdManager reorderings: ", self.mgr.ReadReorderings() );     #/*Returns the number of times reordering has occurred*/
         print("DdManager memory: ", self.mgr.ReadMemoryInUse() );         #/*Returns the memory in use by the manager measured in bytes*/
-        #self.mgr.PrintDebug( dd, n, pr);                                        #/*Prints to the standard output a DD and its statistics: number of nodes, number of leaves, number of minterms*/
+        self.mgr.PrintDebug(dd, n, pr);                                        #/*Prints to the standard output a DD and its statistics: number of nodes, number of leaves, number of minterms*/
 
 
     def initialize_variables(self, names):
@@ -132,114 +125,80 @@ class forward_reachability():
             bool_function = self.mgr.Or(bool_function, self.cube2bdd(cube, names))
         return bool_function
 
-    def parse(self, init_file_name):
+    def parse(self):
         p = parser()
-        [list_of_cubes, list_of_names] = p.open_all_pla(init_file_name)
+        [list_of_cubes, list_of_names] = p.open_all_pla(p.data_location+"init.pla")
         return list_of_cubes, list_of_names
 
 
-    def execute(self):
-        init_file_name = "data/init.pla"
+    def execute_bdd_build(self):
         #list_of_cubes/names are lists of lists, the first row is from the init file.
-        [list_of_cubes, list_of_names] = self.parse("data/init.pla")
+        [list_of_cubes, list_of_names] = self.parse()
 
         initcubes = list_of_cubes[0]
         prenames = list_of_names[0]
         self.initialize_variables(prenames) # Initialize the variables in the init.pla file
         next_state_names = []
+
         for name in prenames:
             next_state_name = name.replace('_', '') # Stripping the double underscore "__" gives us next state variables
             next_state_names.append(next_state_name)
 
         self.initialize_variables(next_state_names) # Initialize the next state variable_names
-
-        ## better names for binit? what is this exactly?
         init_bool_function = self.construct_bdd(initcubes, prenames)
         transition_relation = init_bool_function ############# check this?
+
         for index, name in enumerate(list_of_names[1:]):
-            #next_state_cubes, next_state_names = self.parse(n+".pla")
-            ## better names for bn? what is this exactly?
             next_bool_function = self.construct_bdd(list_of_cubes[index], list_of_names[index])
             transition_relation = self.mgr.Or(transition_relation, next_bool_function)
-        #self.mgr.PrintMinterm(total_function)
-
-
         return init_bool_function, transition_relation
 
-        #edge set = transition relation?
-        # curr_I?
+    def create_abstraction_cube(self):
+        cubeP_names = ["temp1", "temp2", "temp3", "temp4" ]
+        cubeP_cubes = ['1111'] # Not sure what we need for cubeP, but this doesn't break the code.
+        cubeP = self.construct_bdd(cubeP_names, cubeP_cubes)
+        return cubeP
+
+    def states_to_array(self): # the next and current states need to be placed in a repy DDArray
+        for key in self.name2bdd.keys():
+            if "_" in key:
+                #present_state_keys.append(self.name2bdd[key]) # not currently used
+                self.present_state_DDArray.Push(self.name2bdd[key])
+            else:
+                #next_state_keys.append(self.name2bdd[key]) # not currently used
+                self.next_state_DDArray.Push(self.name2bdd[key])
+        return 0
+
+    def step_forward(self, source, transition_relation, cubeP): # Steps forward once
+        temp_next = self.mgr.AndAbstract(source, transition_relation, cubeP) # image of new nodes
+        next_state_image = self.mgr.SwapVariables(temp_next, self.present_state_DDArray, self.next_state_DDArray, 2) # switch the next and present state variables
+        return next_state_image
 
     def find_reachability(self):#, transition_relation):
-        #print("------------------------------------")
-        [source, transition_relation] = self.execute()
-        # repycudd.set_iter_meth(1)
-        #
-        # for node in repycudd.ForeachNprint(self.mgr.VectorSupportSize(source,source))odeIterator(self.mgr, source):
-        #     self.mgr.PrintMinterm(node)
-        # return
-        cubeP_names = ["temp1", "temp2", "temp3", "temp4" ]
-        cubeP_cubes = ['1111']
-        cubeP = self.construct_bdd(cubeP_names, cubeP_cubes)
-        #self.mgr.PrintMinterm(cubeP)
-        total_reachable = source #inital state
+        [source, transition_relation] = self.execute_bdd_build() # builds the bdd from the init.pla files
+        cubeP = self.create_abstraction_cube() # makes cubeP but I'm not sure what this is for right now.
+        total_reachable = source # Inital state
+
+        # Build DD arrays for present and next states
+        self.states_to_array() # updates the global variables present_state_DDArray and next_state_DDArray
+
         while(True):# Until we have no more new states
-            #print('hello')
             new_R = total_reachable
-            #[break_condition, reachable_one_step] = self.step_forward(source, transition_relation, cubeP)
 
-            temp_next = self.mgr.AndAbstract(source, transition_relation, cubeP) ## image of new nodes
+            image = self.step_forward(source, transition_relation, cubeP)
 
-            ## I can't figure out how to use this and I think it's because I still don't understand it.
-            #temp_curr = self.mgr.SwapVariables(temp_next, temp_next, cubeP,4)
-            reachable_one_step = self.mgr.AndAbstract(source, transition_relation, cubeP) ## image of new nodes
+            new_R = self.mgr.Or(image, new_R) # add this onto the reachable set
 
-            new_R = self.mgr.Or(reachable_one_step, new_R)
+            new_source = self.mgr.And(new_R, self.mgr.Not(total_reachable)) # only returns new reached states
 
-            not_R = self.mgr.Not(total_reachable)
+            if (new_source == self.mgr.Zero()):
+                break #end if no new states have been reached
+            total_reachable = new_R # update the reached set
+            source = new_source # change the source to the new reached states
 
-            new_source = self.mgr.And(new_R, not_R) # returns the new present states
-
-            # the condition to break needs to be fixed,
-            num_of_vars = 4 # what is a way to check if the new_source is empty?
-            if (self.mgr.CountMinterm(new_source,num_of_vars) == 0):
-                break
-            total_reachable = new_R
-            source = new_source
-            print("Reached States: ")
-            self.print_dd(source,2,2)
-        self.print_dd(new_R,2,2)
-
-
-        #ps_cp?
-        #ns_cp?
-        #Cubep == return from build bdd
-        #
-
-        #
-        # CubeP = construct_bdd(cubes, names)
-        # while(True):
-        #     step_forward(total_function)
-
-
-    #def step_forward(self, source, transition, cubeP):
-        #temp_next = self.mgr.AndAbstract(source, transition_relation, cubeP) ## image of new nodes
-        #temp_curr = self.mgr.SwapVariables(temp_next, source, source, 4)
-
-
-
-
-
-
-        def create_abstraction_cube(self):
-            ps_cP = []
-            ps_cP.append("111")
-            ns_cP.append("---")
-
-            #Cudd_DumpDot graphs a bdd
-
-
-
-
+        print("Reached States: ")
+        self.print_dd(total_reachable, 2, 2)
+        return 0
 
 def main(args):
     fr = forward_reachability()
@@ -253,72 +212,3 @@ if __name__ == '__main__':
     #import numpy as np
     import re
     sys.exit(main(sys.argv))
-
-
-################## Start BDD builder ##############################
-#
-#
-# def construct_bdd(mgr, f, vars, cubes, N, E):
-#
-#     #define a function for zero case
-#     def bool_zero():
-#         tmp_neg = mgr.Not(vars[j])
-#         mgr.Ref(tmp_neg)
-#         tmp = mgr.And(tmp_neg,cube[i])
-#         mgr.Ref(tmp)
-#         cube[i] = tmp
-#         #break
-#
-#     #define a function for 'one' case
-#     def bool_one():
-#         tmp = mgr.And(mgr,next_state[i])
-#         mgr.Ref(tmp)
-#         cube[i] = tmp
-#         #break
-#         #switch function
-#
-#     def Bool_value(argument):
-#         switch_case = { # dictionary
-#                     0: bool_zero,
-#                         #break,
-#                     1: bool_one
-#         }
-#         func = switch_case.get(arg, lambda: 'switcher failed')
-#         func()
-#
-#
-#     cube = repycudd.DdArray(mgr, E)
-#     repycudd.set_iter_meth(0)
-#     for cube in repycudd.ForeachCubeIterator(m, rel):
-#         cube[i] = mgr.ReadOne()
-#
-#         for j in range(N):
-#                 Bool_value(cubes[i][N - (1 + j)])
-#
-#     f = mgr.ReadLogicZero()
-#
-#     for i in range(E): # or each cube together
-#         tmp = mgr.Or(cube[i],f)
-#         f = tmp
-#
-#     return f
-#
-# def main():
-#     in_vars_num = 3
-#     out_var = 1
-#     mgr = repycudd.DdManager()
-#     inames = { "x0", "x1", "x2" }    #Names for input variables */
-#     onames = { "f" }
-#     x = repycudd.DdArray(mgr, in_vars_num)
-#     for i in range(in_vars_num):
-#         x.Push(mgr.IthVar(i))
-# 		#// x0,x2,x3
-# 	       #x = np.append(x, mgr.NewVar())
-#     cubes_f = { "101" }
-#     f = None
-#     f = construct_bdd(mgr, f, x, cubes_f, 3, 1)
-#
-#     add = mgr.ToAdd(gbm, f) #                   			/*Convert BDD to ADD for display purpose*/
-#     f = add
-#
-# main()
